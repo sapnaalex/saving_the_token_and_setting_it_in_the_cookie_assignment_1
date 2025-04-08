7a54616f987fef5b592e0efeab096aa3207f8bfd
unknown <sapna.alex03@gmail.com>
2025-04-08 12:14:22 +0530

    saving_the_token_and_setting_it_in_the_cookie_assignment_1

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h'});
+  return token;
+
 }
 
 const decrypt = (token) => {
   // return decoded payload
+  try{
+    const decoded =  jwt.verify(token, JWT_SECRET);
+    return decoded;
+  }catch(err){
+    console.log('Token verification failed: ', err.message);
+    return null;
+  }
 }
 
 module.exports = {
   encrypt,
   decrypt
+};
+
+const testPayload = { name: "Test" };
+const token = encrypt(testPayload);
+const result = decrypt(token);
+
+if (result && result.name === "Test") {
+  console.log("Success ✅");
+} else {
+  console.log("Failed ❌");
 }
